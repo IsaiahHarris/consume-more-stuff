a2770b555862801f7cb1546e6a18044a5b2e10df
gchu <gchu647@gmail.com>
2018-08-28 12:30:36 -1000

    finish implementing seeds

--- server/db/seeds/01_add_categories.js
+++ server/db/seeds/01_add_categories.js
@@ -1,14 +1,22 @@
 
 exports.seed = function(knex, Promise) {
-  // Deletes ALL existing entries
-  return knex('categories').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('categories').insert([
-        {id: 1, name: 'vehicles'},
-        {id: 2, name: 'computers'},
-        {id: 3, name: 'appliances'},
-        {id: 4, name: 'furnitures'},
-      ]);
-    });
+  // Delete all entries in items table
+  return knex('items').del()
+    .then(function() {
+      // Delete all entries in users table
+      return knex('users').del()
+        then(function() {
+          // Delete all entries in categories table
+          return knex('categories').del()
+          .then(function () {
+            // Inserts seed entries for categories
+            return knex('categories').insert([
+              {id: 1, name: 'vehicles'},
+              {id: 2, name: 'computers'},
+              {id: 3, name: 'appliances'},
+              {id: 4, name: 'furnitures'},
+            ]);
+          });
+        })
+    })
 };
