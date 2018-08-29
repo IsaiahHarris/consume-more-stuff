1d9677830073c7e1264de4d92f38c00ccc90ea88
gchu <gchu647@gmail.com>
2018-08-28 14:12:45 -1000

    fetch all items in items route using GET

--- server/routes/items.js
+++ server/routes/items.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const router = express.Router();
+const Items = require('../db/models/Item');
 
-router.get('/', (req, res) => {
-  res.send('Welcome to Items.js');
-});
+router.route('/')
+  .get((req, res) => { // Fetches all the items
+    return Items.fetchAll() // Need to include a where depending on  category
+      .then(items => {
+        res.json(items);
+      })
+      .catch(err => {
+        res.json({ 'error': err.message })
+      });
+    // res.send('smoke test 2 items');
+  });
 
 module.exports = router;
