a7fa9aee4561485f24d893414178a553cc461e41
gchu <gchu647@gmail.com>
2018-08-29 09:52:25 -1000

    add ids to seed datas

--- server/db/seeds/01_add_categories.js
+++ server/db/seeds/01_add_categories.js
@@ -1,13 +1,22 @@
 
 exports.seed = function (knex, Promise) {
-  return knex('categories').del()
-    .then(function () {
-      // Inserts seed entries for categories
-      return knex('categories').insert([
-        { name: 'vehicles' },
-        { name: 'computers' },
-        { name: 'appliances' },
-        { name: 'furnitures' },
-      ]);
-    });
+  // Delete entries in the items table
+  return knex('items').del()
+    .then(function() {
+      // Delete entries in the users table
+      return knex('users').del()
+        .then(function () { 
+          // Delete entries in categories table
+          return knex('categories').del()
+          .then(function () {
+            // Inserts seed entries for categories
+            return knex('categories').insert([
+              { id: 1, name: 'vehicles' },
+              { id: 2, name: 'computers' },
+              { id: 3, name: 'appliances' },
+              { id: 4, name: 'furnitures' },
+            ]);
+          });
+        })
+    })
 };
