53fad041a902149ef52f803ae9ef8076928a3aa9
Isaiah Harris <harrisfambam@gmail.com>
2018-08-28 19:41:51 -1000

    fix seeds

--- server/db/seeds/02_add_user_statuses.js
+++ server/db/seeds/02_add_user_statuses.js
@@ -1,12 +1,12 @@
 
-exports.seed = function(knex, Promise) {
+exports.seed = function (knex, Promise) {
   // Deletes ALL existing entries
   return knex('user_statuses').del()
     .then(function () {
       // Inserts seed entries
       return knex('user_statuses').insert([
-        {id: 1, name: 'active'},
-        {id: 2, name: 'inactive'},
+        { name: 'active' },
+        { name: 'inactive' },
       ]);
     });
 };
--- server/db/seeds/03_add_item_statuses.js
+++ server/db/seeds/03_add_item_statuses.js
@@ -1,12 +1,12 @@
 
-exports.seed = function(knex, Promise) {
+exports.seed = function (knex, Promise) {
   // Deletes ALL existing entries
   return knex('item_statuses').del()
     .then(function () {
       // Inserts seed entries
       return knex('item_statuses').insert([
-        {id: 1, name: 'published'},
-        {id: 2, name: 'sold'},
+        { name: 'published' },
+        { name: 'sold' },
       ]);
     });
 };
