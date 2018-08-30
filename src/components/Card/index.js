47de30246b5caca8e82529191c59d9dd2e5cf2fb
Bronson Avila <bronson.avila@gmail.com>
2018-08-29 23:37:48 -1000

    Configure "Card" component to truncate title text

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import './Card.css'
+import './Card.css';
 import { connect } from 'react-redux';
-import { loadCards } from '../../actions'
+import { loadCards } from '../../actions';
 
 class Card extends React.Component {
   constructor(props) {
-    super(props)
-
+    super(props);
   }
 
   // componentDidMount() {
@@ -14,27 +13,33 @@ class Card extends React.Component {
   // }
 
   render() {
-
     const styles = {
-      backgroundImage: "url(" + this.props.photo + ")",
+      backgroundImage: 'url(' + this.props.photo + ')',
       backgroundSize: 'cover',
       backgroundRepeat: 'no-repeat',
       backgroundPosition: 'center center',
       height: '100px',
-      width: '120px',
-    }
+      width: '120px'
+    };
 
     return (
       <div className="main-card-container">
         <div className="card-container">
           <div className={this.props.condition}>{this.props.condition}</div>
-          <div style={styles} className="photo"></div>
-          <a href="">{this.props.title}</a>
+          <div style={styles} className="photo" />
+          <a href="">{truncateText(this.props.title, 32)}</a>
           <div className="price">{this.props.price}</div>
         </div>
       </div>
-    )
+    );
   }
+}
+
+function truncateText(str, maxLength) {
+  // Source (Stack Overflow): https://goo.gl/BDKhqs
+  return str.length <= maxLength
+    ? str
+    : str.substr(0, str.lastIndexOf(' ', maxLength)) + ' \u2026';
 }
 
 // const mapStateToProps = state => {
