59c27d68b9364f9a518cee4f0e0899deaf2027ef
Isaiah Harris <harrisfambam@gmail.com>
2018-08-30 19:21:27 -1000

    add add button to home

--- src/components/ItemDetail/index.js
+++ src/components/ItemDetail/index.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import { connect } from 'react-redux';
-import { loadCard } from '../../actions';
 import Button from '../Button';
 import './itemDetail.css';
 class ItemDetail extends React.Component {
--- src/components/ItemNew/index.js
+++ src/components/ItemNew/index.js
@@ -2,14 +2,58 @@ import React, { Component } from 'react';
 
 import './ItemNew.css';
 import Button from '../Button';
-
-class ItemNew extends Component {
+import React from 'react';
+import './itemDetail.css';
+class ItemNew extends React.Component {
   constructor(props) {
-    super(props);
+    super(props)
+
   }
 
   render() {
-    return;
+
+    const {
+      title,
+      price,
+      manufacturer,
+      model,
+      dimensions,
+      details,
+      photo,
+      seller,
+      category,
+      status,
+      condition
+    } = this.props.location.state
+    const styles = {
+      backgroundImage: 'url(' + photo + ')',
+      backgroundSize: 'contain',
+      backgroundRepeat: 'no-repeat',
+      backgroundPosition: 'center center',
+      height: '20vh',
+      width: '150px',
+      paddingTop: '3%'
+    };
+    return (
+      <div className="item-detail-view-container">
+        <div style={styles} className="item-photo"></div>
+        <div className="item-header">
+          <div className="item-title">{title}</div>
+          <div className="seller">{seller}</div>
+        </div>
+        <div className="add-button-container">
+          <Button label='Add Photo' />
+        </div>
+        <div className="item-details-container">
+          <div className="item-price">Price: {price}</div>
+          <div className="item-condition">Condition: {condition}</div>
+          <div className="item-manufacturer">Make: {manufacturer}</div>
+          <div className="item-model">Model: {model}</div>
+          <div className="item-dimensions">dimensions: {dimensions}</div>
+          <div className="item-note">Note: {details}</div>
+        </div>
+      </div>
+    )
   }
 }
 
