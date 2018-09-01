ca88ab6408b10b0290a60c43e39a1a2a776c2170
gchu <gchu647@gmail.com>
2018-09-01 12:39:14 -1000

    change MainContainer back to stateless, and css issue with items/new

--- src/components/MainContainer/index.js
+++ src/components/MainContainer/index.js
@@ -9,45 +9,33 @@ import ItemDetail from '../ItemDetail';
 import ItemNew from '../ItemNew';
 import Login from '../Login';
 
-class MainContainer extends Component {
-  constructor(props) {
-    super(props);
-  }
+const MainContainer = () =>  {
 
-  render() {
-    console.log('This is current logged in user', this.props.user);
+  return (
+    <div className="MainContainer">
+      <Sidebar />
+      <Switch>
+        {/* NOTE: Change "Body" to something more descriptive, e.g., Home Page */}
+        <Route
+          exact path="/"
+          component={Body}
+        />
+        <Route
+          exact path="/items/new"
+          component={ItemNew}
+        />
+        <Route
+          exact path="/items/:id"
+          component={ItemDetail}
+        />
+        <Route
+          exact path="/login"
+          component={Login}
+        />
+      </Switch>
+    </div>
+  );
 
-    return (
-      <div className="MainContainer">
-        <Sidebar />
-        <Switch>
-          {/* NOTE: Change "Body" to something more descriptive, e.g., Home Page */}
-          <Route
-            exact path="/"
-            component={Body}
-          />
-          <Route
-            exact path="/items/new"
-            component={ItemNew}
-          />
-          <Route
-            exact path="/items/:id"
-            component={ItemDetail}
-          />
-          <Route
-            exact path="/login"
-            component={Login}
-          />
-        </Switch>
-      </div>
-    );
-  }
 }
 
-const mapStateToProps = state => {
-  return {
-    user: state.usersList
-  }
-}
-
-export default connect(mapStateToProps)(MainContainer);
+export default MainContainer;
