4bfb40bafda819d3bc307780351d517e123eedd8
Isaiah Harris <harrisfambam@gmail.com>
2018-09-01 16:44:35 -1000

    add back button to item/new

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -14,18 +14,7 @@ class Header extends React.Component {
       <div className="header-container">
         <img src="https://i.imgur.com/34axnfY.png" className="logo" alt="" />
         <SearchBar classNameLabel="wrap" />
-        <div className="button-container">
-          <div className="header-button">
-            <Button label="Login" />
-          </div>
-          <div className="header-button">
-            <Link to={'/'}>
-              <Button label="Back" />
-            </Link>
-          </div>
-
-        </div>
-
+        <Button label="Login" />
       </div >
     )
   }
