7465924618f4305d634558c9505c853c7c4fc5a7
gchu <gchu647@gmail.com>
2018-09-01 15:19:51 -1000

    fixed connect router issue in MainContainer

--- src/components/MainContainer/index.js
+++ src/components/MainContainer/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Route, Switch, withRouter } from 'react-router-dom';
 
 import './MainContainer.css';
 import Sidebar from '../Sidebar';
@@ -9,33 +9,45 @@ import ItemDetail from '../ItemDetail';
 import ItemNew from '../ItemNew';
 import Login from '../Login';
 
-const MainContainer = () =>  {
+class MainContainer extends Component {
+  constructor(props) {
+    super(props);
+  }
 
-  return (
-    <div className="MainContainer">
-      <Sidebar />
-      <Switch>
-        {/* NOTE: Change "Body" to something more descriptive, e.g., Home Page */}
-        <Route
-          exact path="/"
-          component={Body}
-        />
-        <Route
-          exact path="/items/new"
-          component={ItemNew}
-        />
-        <Route
-          exact path="/items/:id"
-          component={ItemDetail}
-        />
-        <Route
-          exact path="/login"
-          component={Login}
-        />
-      </Switch>
-    </div>
-  );
+  render() {
+    console.log('This is current logged in user', this.props.user);
 
+    return (
+      <div className="MainContainer">
+        <Sidebar />
+        <Switch>
+          {/* NOTE: Change "Body" to something more descriptive, e.g., Home Page */}
+          <Route
+            exact path="/"
+            component={Body}
+          />
+          <Route
+            exact path="/items/new"
+            component={ItemNew}
+          />
+          <Route
+            exact path="/items/:id"
+            component={ItemDetail}
+          />
+          <Route
+            exact path="/login"
+            component={Login}
+          />
+        </Switch>
+      </div>
+    );
+  }
 }
 
-export default MainContainer;
+const mapStateToProps = state => {
+  return {
+    user: state.usersList
+  }
+}
+
+export default withRouter(connect(mapStateToProps)(MainContainer));
