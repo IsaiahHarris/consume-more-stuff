906626e58f98f909170163ff449ec45cd7c16e1c
Bronson Avila <bronson.avila@gmail.com>
2018-09-05 16:58:42 -1000

    Remove obsolete reference to "s3Upload" route

--- server/routes/index.js
+++ server/routes/index.js
@@ -5,12 +5,10 @@ const conditions = require('./conditions');
 const categories = require('./categories');
 const items = require('./items');
 const auth = require('./auth')
-const s3Upload = require('./s3Upload');
 
 router.use('/conditions', conditions)
 router.use('/categories', categories);
 router.use('/items', items);
 router.use('/', auth);
-router.use('/', s3Upload);
 
 module.exports = router;
