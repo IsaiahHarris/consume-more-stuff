f6178fa1ecea68564733282a28952e75a1d77454
Isaiah Harris <harrisfambam@gmail.com>
2018-09-05 18:41:06 -1000

    fix merge conflicts

--- server/routes/index.js
+++ server/routes/index.js
@@ -5,11 +5,11 @@ const conditions = require('./conditions');
 const categories = require('./categories');
 const items = require('./items');
 const auth = require('./auth')
+const user = require('./user')
 router.use('/conditions', conditions)
 router.use('/categories', categories);
 router.use('/items', items);
 router.use('/', auth);
-
 router.use('/user', user)
 
 module.exports = router;
