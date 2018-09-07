16c89cbdb72d4e199edb9c42952aa9bf179455a4
Bronson Avila <bronson.avila@gmail.com>
2018-09-07 12:42:36 -1000

    Remove unnecessary console.log()

--- server/routes/user.js
+++ server/routes/user.js
@@ -34,10 +34,9 @@ router.put('/settings', (req, res) => {
     });
 });
 
-// This routes fetches all the items of one user
+// Fetch all items of one user:
 router.get('/:user_id/items', (req, res) => {
   const userId = req.params.user_id;
-  console.log('userId', userId);
 
   return Item.query(qb => {
     qb.where({ seller_id: userId }).andWhere({ deleted_at: null });
