b851fb7142c3f192f3ef04738d9a6525f7f3f50f
Bronson Avila <bronson.avila@gmail.com>
2018-09-06 19:41:12 -1000

    Remove unnecessary white space following spread operators (re: warning)

--- src/reducers/userItems/publishList.js
+++ src/reducers/userItems/publishList.js
@@ -5,10 +5,10 @@ const initialState = [];
 const publishList = (state = initialState, action) => {
   switch (action.type) {
     case LOAD_CARDS_BY_PUBLISHED:
-      return [... action.publishCards];
+      return [...action.publishCards];
     default:
       return state;
   }
-}
+};
 
 export default publishList;
