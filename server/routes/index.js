9a0e23d0e161782af69ab414035e244700b406ac
Bronson Avila <bronson.avila@gmail.com>
2018-09-06 18:40:55 -1000

    Import "itemStatuses" route into main routes index file

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,11 +3,13 @@ const router = express.Router();
 
 const conditions = require('./conditions');
 const categories = require('./categories');
+const itemStatuses = require('./itemStatuses');
 const items = require('./items');
-const auth = require('./auth')
+const auth = require('./auth');
 
-router.use('/conditions', conditions)
+router.use('/conditions', conditions);
 router.use('/categories', categories);
+router.use('/itemStatuses', itemStatuses);
 router.use('/items', items);
 router.use('/', auth);
 
