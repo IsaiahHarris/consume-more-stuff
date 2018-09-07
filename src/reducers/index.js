b1c51425b2445f6e17206bf376a710ab28afa854
Bronson Avila <bronson.avila@gmail.com>
2018-09-06 18:44:14 -1000

    Import "itemStatusesList" into main reducer index file

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,10 +3,12 @@ import cardsList from './cardsList';
 import conditionsList from './conditionsList';
 import categoriesList from './categoriesList';
 import usersList from './usersList';
+import itemStatusesList from './itemStatusesList';
 
 export default combineReducers({
   categoriesList,
   cardsList,
   conditionsList,
-  usersList
-})
+  usersList,
+  itemStatusesList
+});
