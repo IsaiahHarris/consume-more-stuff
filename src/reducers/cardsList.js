aaa857caff4c3f8d9417ec563d4298885f7e4c9d
Bronson Avila <bronson.avila@gmail.com>
2018-09-06 19:41:43 -1000

    Remove unnecessary arrow function (re: warning)

--- src/reducers/cardsList.js
+++ src/reducers/cardsList.js
@@ -1,43 +1,28 @@
-import { LOAD_CARDS, LOAD_CARD, ADD_CARD, EDIT_CARD, LOAD_CARDS_BY_CATEGORY } from '../actions';
+import {
+  LOAD_CARDS,
+  LOAD_CARD,
+  ADD_CARD,
+  EDIT_CARD,
+  LOAD_CARDS_BY_CATEGORY
+} from '../actions';
 
-const initialState = [
-  // {
-  //   title: 'soccer shoes',
-  //   price: 'teeth',
-  //   manufacturer: 'nike',
-  //   model: 'vapor x',
-  //   dimensions: 'M8XM8',
-  //   details: 'make you run fast',
-  //   image_url: 'https://i.ebayimg.com/images/g/BscAAOSweW5VDMoM/s-l300.jpg',
-  //   seller_id: 1,
-  //   category_id: 1,
-  //   item_status_id: 1,
-  //   condition_id: 1
-  // }
-]
+const initialState = [];
 
 const cardsList = (state = initialState, action) => {
   switch (action.type) {
     case LOAD_CARDS:
-      return [...action.cards]
+      return [...action.cards];
     case LOAD_CARDS_BY_CATEGORY:
-      return [...action.cardsByCategory]
+      return [...action.cardsByCategory];
     case LOAD_CARD:
-      return [action.card]
+      return [action.card];
     case ADD_CARD:
-      return [...state, action.card]
+      return [...state, action.card];
     case EDIT_CARD:
-      state.map(card => {
-        if (card.id === Number(action.editCard.id)) {
-          state.splice(state.indexOf(card), 1)
-          state.push(action.editCard);
-        }
-      })
-      return [...state]
+      return [...state];
     default:
       return state;
   }
-}
-
+};
 
 export default cardsList;
